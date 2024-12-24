39e16efe310d27c78825ac2c840ef309588dba52
Mehdi Kibous <mkibous@e2r5p11.1337.ma>
2024-12-24 10:26:51 +0100

    06

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkibous <mkibous@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 15:16:05 by mkibous           #+#    #+#             */
+/*   Updated: 2024/12/22 15:19:08 by mkibous          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+ScalarConverter::ScalarConverter(){}
+void ScalarConverter::convert(std::string str) {}
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkibous <mkibous@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 14:18:44 by mkibous           #+#    #+#             */
+/*   Updated: 2024/12/22 15:15:55 by mkibous          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+class ScalarConverter
+{
+private:
+ScalarConverter();
+public:
+    static void convert(std::string str);
+};
